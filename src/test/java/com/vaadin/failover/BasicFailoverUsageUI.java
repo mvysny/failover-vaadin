87c77f3fe280a071452fbbc985aaf64a595636fc
Martin Vysny <mavi@vaadin.com>
2017-01-27 17:23:09 +0200

    Added tests for reconnection logic dialog configuration

--- src/test/java/com/vaadin/failover/BasicFailoverUsageUI.java
+++ src/test/java/com/vaadin/failover/BasicFailoverUsageUI.java
@@ -21,10 +21,13 @@ public class BasicFailoverUsageUI extends AbstractTest {
 
     @Override
     public Component getTestComponent() {
-        final List<String> urls = Arrays.asList("http://197.100.100.100/company", "http://197.100.100.101/company", "http://197.100.100.102/company", "https://vaadin.com/company");
+        final List<String> urls = Arrays.asList("http://197.100.100.100", "http://197.100.100.101", "http://197.100.100.102", "https://vaadin.com");
         final FailoverReconnectExtension reconnectExtension = FailoverReconnectExtension.addTo(UI.getCurrent());
         reconnectExtension.setUrls(urls);
         reconnectExtension.setStatusLabel(status);
+        reconnectExtension.setInfinite(false);
+        reconnectExtension.setRandomRobin(false);
+        reconnectExtension.setPingMillis(2000);
         getReconnectDialogConfiguration().setDialogText("Can't connect to the server. The network may be down, or the server has crashed. Press the 'Reconnect' button to try to connect to fallback server.");
         final VerticalLayout vl = new VerticalLayout();
         vl.addComponent(new Label("Kill the server and click the button: the browser should automatically redirect to " + urls));
